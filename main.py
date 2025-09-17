152275b0c8b8d49fa82925ceed5b238bcbd273d1
Jay Will <williamsjorden685@gmail.com>
2025-09-17 12:51:22 -0400

    Hello

---
+++ main.py
@@ -0,0 +1,6 @@
+game_name = ("Totally,Real!")
+print("Welcome to", game_name) # TODO, print greetings with game name 
+print("========================")
+print("Before we begin, what is your character name?")
+character_name = input() 
+print("Great, ", character_name,"!", " Lets begin the adventure!", sep="")
