e42fe5d4d73b0d750bd5bb41edfbb21ba4491148
4418-sudo <jordennwilliams05@gmail.com>
2025-10-17 17:53:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import random 
+
 game_name = ("Totally,Real")
-print("Welcome to ", game_name, "!", sep = "") # TODO, print greetings with game name 
-print("========================")
-character_name = input("Before we begin, what is your character name? \n> ") 
-print("Great, ", character_name,"!", " Lets begin the adventure!", sep="")
+
+print(f"Welcome to {game_name}!"))
+print("=" * (len(game_name) + 15)) 
+
+print("Before we begin, what is your character's name ?")
+name = input(">")
+
+print("Great, {name}! Let's begin the adventure!")
+
+player = {
+    "name": name,
+  "health": 100,
+  "coin": 0 
+}
+
+events = ["Find a coin", "Meet a monster", "do nothing"]
+
+event = random.choice(events) 
+print(f"While exploring, you {event}!")
+
+if event == "find a coin": 
+    player['coin']: +=1 
+  print(f'{player['name']} found a coin, {player['name']} now has {player['coin']}coins.')
+elif event == 'meet a monster':
+  player["health"] -= 10
+  print(f'{player['name']} got hurt during the combat with monster, health is now {player['health']}.')
+else:
+  print("Nothing happened this time.")
+  
